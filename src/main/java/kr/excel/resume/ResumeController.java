f2fe22278694a3ac32823577689d4f5df96fb03f
admin <gkaeogus7@gmail.com>
2023-08-07 11:09:56 +0900

    20230807

--- src/main/java/kr/excel/resume/ResumeController.java
+++ src/main/java/kr/excel/resume/ResumeController.java
@@ -4,9 +4,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFRichTextString;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.*;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -66,6 +64,68 @@ public class ResumeController {
             g2d.dispose();
 
             // 조절된 이미지를 바이트 배열로 변환합니다.
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(resizedBufferedImage, "png", baos);
+            byte[] imageBytes = baos.toByteArray();
+            int imageIndex = workbook.addPicture(imageBytes, Workbook.PICTURE_TYPE_PNG);
+
+            // Drawing 객체를 생성하고 이미지를 삽입합니다.
+            XSSFDrawing drawing = (XSSFDrawing) sheet.createDrawingPatriarch();
+            XSSFClientAnchor anchor = new XSSFClientAnchor(0, 0, 0, 0, 0, 1, 1, 2);
+            drawing.createPicture(anchor, imageIndex);
+
+            // 이미지가 삽입된 행의 높이와 열의 너비를 조정합니다.
+            // 96은 화면의 DPI(Dots Per Inch, 인치당 노드의 수)
+            // Excel에서 셀의 높이는 포인트(point) 단위로 표시( 1포인트는 1/72 인치입니다)
+            dataRow.setHeightInPoints(newHeight*72/96); // 픽셀을 point로변경
+            // 8이란 값은, 엑셀에서 사용되는 기본 문자 폭의 값
+            // 엑셀에서는 한 개의 문자가 차지하는 너비를 1/256 단위로 계산
+            int columnWidth = (int) Math.floor(((float)newWidth / (float) 8) *256);
+            sheet.setColumnWidth(0, columnWidth);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        dataRow.createCell(1).setCellValue(personInfo.getName());
+        dataRow.createCell(2).setCellValue(personInfo.getEmail());
+        dataRow.createCell(3).setCellValue(personInfo.getAddress());
+        dataRow.createCell(4).setCellValue(personInfo.getPhoneNumber());
+        dataRow.createCell(5).setCellValue(personInfo.getBirthDate());
+
+        // 학력사항 헤더 생성
+        int educationStartRow = 3;
+        Row educationHeaderRow = sheet.createRow(educationStartRow - 1);
+        educationHeaderRow.createCell(0).setCellValue("졸업년도");
+        educationHeaderRow.createCell(1).setCellValue("학교명");
+        educationHeaderRow.createCell(2).setCellValue("전공");
+        educationHeaderRow.createCell(3).setCellValue("졸업여부");
+
+        // 학력사항 데이터 삽입
+        int educationRowNum = educationStartRow;
+        for (Education education : educationList) {
+            Row educationDataRow = sheet.createRow(educationRowNum++);
+            educationDataRow.createCell(0).setCellValue(education.getGraduationYear());
+            educationDataRow.createCell(1).setCellValue(education.getSchoolName());
+            educationDataRow.createCell(2).setCellValue(education.getMajor());
+            educationDataRow.createCell(3).setCellValue(education.getGraduationStatus());
+        }
+
+        // 경력사항 헤더 생성
+        int careerStartRow = educationRowNum + 1;
+        Row careerHeaderRow = sheet.createRow(careerStartRow - 1);
+        careerHeaderRow.createCell(0).setCellValue("근무기간");
+        careerHeaderRow.createCell(1).setCellValue("근무처");
+        careerHeaderRow.createCell(2).setCellValue("담당업무");
+        careerHeaderRow.createCell(3).setCellValue("근속연수");
+
+        // 경력사항 데이터 삽입
+        int careerRowNum = careerStartRow;
+        for (Career career : careerList) {
+            Row careerDataRow = sheet.createRow(careerRowNum++);
+            careerDataRow.createCell(0).setCellValue(career.getWorkPeriod());
+            careerDataRow.createCell(1).setCellValue(career.getCompanyName());
+            careerDataRow.createCell(2).setCellValue(career.getJobTitle());
+            careerDataRow.createCell(3).setCellValue(career.getEmploymentYears());
         }
     }
 
